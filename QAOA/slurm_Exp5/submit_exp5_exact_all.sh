c573fc05028d8b0bb0fa08dc4a6d4e813ff7a33c
Julian Klitzke <julian@julians-macbook-air-2.local.wired.unimaas.local>
2026-07-24 13:08:12 +0200

    slurm files exp5

---
+++ QAOA/slurm_Exp5/submit_exp5_exact_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euo pipefail
+
+sbatch slurm_Exp5/exact/complete_2to12/exp5_exact_complete_2to12.slurm
+sbatch slurm_Exp5/exact/cycle_3to12/exp5_exact_cycle_3to12.slurm
+sbatch slurm_Exp5/exact/path_2to12/exp5_exact_path_2to12.slurm
+sbatch slurm_Exp5/exact/bipartite_454/exp5_exact_bipartite_454.slurm
+sbatch slurm_Exp5/exact/planar_clawfree_193/exp5_exact_planar_clawfree_193.slurm
+sbatch slurm_Exp5/exact/regular_388/exp5_exact_regular_388.slurm
